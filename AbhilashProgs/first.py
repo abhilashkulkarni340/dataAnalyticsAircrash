5d0a1298958e1cb9e7916d4edc064bb0ed9a2887
Abhilash Sachin Kulkarni <abhilashkulkarni340@hotmail.com>
2017-11-25 00:33:51 +0530

    Update first.py

--- AbhilashProgs/first.py
+++ AbhilashProgs/first.py
@@ -11,67 +11,94 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 
+
+"""
+Dataset loading section
+"""
+
 #Function for Data ingestion stage using read_csv
 def readData():
     try:
         dataset=pd.read_csv('./Airplane_Crashes_and_Fatalities_Since_1908.csv') #storing the data in a DataFrame
-        print("Dataset loaded successfully.")
+        print("\nDataset loaded successfully.")
         return dataset
     except FileNotFoundError:
-        print("The file does not exist.")
+        print("\nThe file does not exist.")
     
     #print(crashds.head(2))
+  
+    
+"""
+Data cleaning section
+"""
 
 #Function for Data cleaning stage
 def cleanData(dataset):
+    print("\nCleaning data now...")
     del dataset['Summary']  #Summary column is dropped
     
     #removing invalid values
     ds1=dataset.isnull().any()
 #    print(ds1)
     if True in ds1.values:  #if there are invalid value drop them
-        print("There are invalid values in the dataset. Deleting invalid values...")
+        print("\nThere are invalid values in the dataset. Deleting invalid values...")
         dataset=dataset.dropna()
-        print("Invalid values have been dropped.\n")
+        print("\nInvalid values have been dropped.\n")
 #        ds1=dataset.isnull().any()
 #        print(ds1)
     else:   #if there are no invalid values
-        print("There are no invalid values in the dataset.\n")
+        print("\nThere are no invalid values in the dataset.\n")
         
     #changing the format and type of values in Date column
-    shape=dataset.shape
-    for x in range(shape[0]):
-        dataset.iloc[x,0]=datetime.datetime.strptime(dataset.iloc[x,0], "%m/%d/%Y").date()
+    dataset['Date']=pd.to_datetime(dataset['Date'],format="%m/%d/%Y")
     
-    print("Dataset has been cleaned.")
+    print("\nDataset has been cleaned.")
     #returning dataset
     return dataset
 
+
 """
-Selecting Columns Section
+Data Transformation Section
 """
 
 #Function to return columns of the dataset
 def getColumns(dataset,col,rowStart,rowEnd):
-    return dataset[col].iloc[rowStart:rowEnd].to_string(index=False)
+    print("\nIn getColumns method....  ")
+    return dataset[col].iloc[rowStart:rowEnd]
 
 #Function to return dataset filtered by dates
-def filterDs(dataset,frm,to):
+def filterByDates(dataset,frm,to):
+    print("\nIn filterByDate method....  ")
     filter_1=dataset['Date']>frm
-    filter_2=dataset['Date']>to
+    filter_2=dataset['Date']<to
     return dataset[filter_1 & filter_2]
+
+#Function to return dataset filtered by Operator
+def filterByOperator(dataset,operator):
+    print("\nIn filterByOperator method....  ")
+    filter_op=dataset['Operator'].str.contains(operator)
+    return dataset[filter_op]
+
+
 
 """
 Descriptive Statistics Section
 """
+#Function to get dataset details
+def desDataset(dataset,info):
+    print("\nIn desDataset method....  ")
+    if info=="shape":
+        return dataset.shape
 
 #Function to describe the columns
 def desColumns(dataset,col):
+    print("\nIn desColumns method....  ")
     desc=dataset[col].describe()
     return desc
 
 #Function for displaying data regarding the data set
 def defDataset(dataset):
+    print("\nIn defDataset method....  ")
     #print(dataset.head())
     #print(dataset.tail())
     shape=dataset.shape
@@ -82,6 +109,7 @@ def defDataset(dataset):
 
 #Function to return column and rows of the dataset
 def retData(dataset,cols,rows=0):   #cols will be a list of columns selected
+    print("\nIn retData method....  ")
     columns=dataset.columns
     for x in columns:
         print(dataset[x])
